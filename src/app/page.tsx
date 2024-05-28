7921ceaa49a6df26b9717fef9cb07e4818071a18
Aryan Gupta <aryangupta@Aryans-MacBook-Pro.local>
2024-05-28 15:05:51 -0700

    added contact images and used Image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Linkedin from "./images/linkedin.png"
+import GitHub from "./images/github.png"
 
 export default function Home() {
   return (
@@ -29,38 +31,34 @@ export default function Home() {
       </div> */}
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group relative rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold relative">
-            Skills{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-            <div className="absolute left-0 top-32 mt-2 hidden w-96 p-4 bg-white border border-gray-300 rounded-lg shadow-lg group-hover:block dark:bg-neutral-800 dark:border-neutral-700">
-              <div className="grid grid-cols-2 gap-4">
-                <div>
-                  <h3 className="font-normal mb-2">Languages</h3>
-                  <img src="image1_url" alt="Java" className="m-0 max-w-[30ch] text-sm opacity-50" />
-                  <img src="image2_url" alt="Python" className="m-0 max-w-[30ch] text-sm opacity-50" />
-                  <img src="image3_url" alt="JavaScript" className="m-0 max-w-[30ch] text-sm opacity-50" />
-                </div>
-                <div>
-                  <h3 className="font-normal mb-2">Frameworks</h3>
-                  <img src="framework1_url" alt="React" className="m-0 max-w-[30ch] text-sm opacity-50" />
-                  <img src="framework2_url" alt="Vue" className="m-0 max-w-[30ch] text-sm opacity-50" />
-                  <img src="framework3_url" alt="Angular" className="m-0 max-w-[30ch] text-sm opacity-50" />
-                </div>
+      <div
+        className="group relative rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        rel="noopener noreferrer"
+      >
+        <h2 className="mb-3 text-2xl font-semibold relative">
+          Skills{" "}
+          <div className="absolute left-0 top-24 mt-2 hidden w-96 p-4 bg-white border border-gray-300 rounded-lg shadow-lg group-hover:block dark:bg-neutral-800 dark:border-neutral-700">
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <h3 className="m-0 text-sm">Languages</h3>
+                <Image src="/image1_url" alt="Java" className="m-0 max-w-[30ch] text-sm opacity-50" width={1} height={1}/>
+                <Image src="/image2_url" alt="Python" className="m-0 max-w-[30ch] text-sm opacity-50" width={1} height={1}/>
+                <Image src="/image3_url" alt="JavaScript" className="m-0 max-w-[30ch] text-sm opacity-50" width={1} height={1}/>
+              </div>
+              <div>
+                <h3 className="m-0 text-sm">Frameworks</h3>
+                <Image src="/framework1_url" alt="React" className="m-0 max-w-[30ch] text-sm opacity-50" width={1} height={1}/>
+                <Image src="/framework2_url" alt="Vue" className="m-0 max-w-[30ch] text-sm opacity-50" width={1} height={1}/>
+                <Image src="/framework3_url" alt="Angular" className="m-0 max-w-[30ch] text-sm opacity-50" width={1} height={1}/>
               </div>
             </div>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Tech stacks I am familiar with.
-          </p>
-        </a>
+          </div>
+        </h2>
+        <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          Tech stacks I am familiar with.
+        </p>
+      </div>
+
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -95,7 +93,7 @@ export default function Home() {
           </p>
         </a>
 
-        <a
+        {/* <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -110,7 +108,48 @@ export default function Home() {
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
             Get in touch with me?
           </p>
-        </a>
+        </a> */}
+
+        <div
+          className="group relative rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold relative">
+            Contact{" "}
+            <div className="absolute left-0 top-24 mt-2 hidden w-96 p-4 bg-white border border-gray-300 rounded-lg shadow-lg group-hover:block dark:bg-neutral-800 dark:border-neutral-700">
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <h3 className="m-0 max-w-[30ch] text-sm">Contact:</h3>
+                  <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                    +1 (602)326-7916
+                  </p>
+                </div>
+                <div>
+                  <h3 className="m-0 max-w-[30ch] text-sm">Email:</h3>
+                  <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                    aryang10@illinois.edu
+                  </p>
+                </div>
+                <div className="flex items-center">
+                  <h3 className="m-0 text-sm">LinkedIn:</h3>
+                  <a href="https://www.linkedin.com/in/agupt295/" target="_blank" rel="noopener noreferrer" className="ml-2">
+                    <Image src={Linkedin} alt="LinkedIn" className="w-6 h-6" width={1} height={1}/>
+                  </a>
+                </div>
+                <div className="flex items-center">
+                  <h3 className="m-0 text-sm">GitHub:</h3>
+                  <a href="https://github.com/agupt295" target="_blank" rel="noopener noreferrer" className="ml-2">
+                    <Image src={GitHub} alt="GitHub" className="w-6 h-6" width={1} height={1}/>
+                  </a>
+                </div>
+                {/* Add images of Linkedin and Github */}
+              </div>
+            </div>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Tech stacks I am familiar with.
+          </p>
+        </div>
       </div>
 
       {/* <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
